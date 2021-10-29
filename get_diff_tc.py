b08bfe9f9219ba6b4f6b3ed7706dad0143e438fd
tomorin <tanaka.t.bj@gmail.com>
2021-10-29 17:49:44 +0900

    Some member function is added.

--- get_diff_tc.py
+++ get_diff_tc.py
@@ -4,10 +4,12 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 import os
+import sys
 
 
 class TcFromTcandJmode:
-    """Gets and deals with 2 Tc (Curie temp.) values from tc mode and j mode in AkaiKKR code.
+    """Gets and deals with 2 Curie temperature (curie_temp in the script variables) values
+    from tc mode and j mode in AkaiKKR code.
 
     It is assumed that the executed directory should be like below:
     executed dir
@@ -30,32 +32,48 @@ class TcFromTcandJmode:
         self.filepath_tc_mode = os.getcwd() + dirname_tc + "/" + filename
         self.filepath_j_mode = os.getcwd() + dirname_j + "/" + filename
 
-    def get_tc(self, filepath):
+    def get_curie_temp(self, filepath):
         with open(filepath, mode='r', encoding='utf-8') as file:
             line_list = file.readlines()
 
         for line in line_list:
             if self.search_keyword in line:
-                tc_value = line.split()[self.index_number]
+                curie_temp = line.split()[self.index_number]
         # check whether tc_value is set or not
         try:
-            tc_value
+            curie_temp
         except NameError:
             print("Failed to catch Tc value. Check the output file or script file.")
+            sys.exit()
 
         # The default format of Tc value in AkaiKKR is "xxxxx.xxxK", thus remove "K" in the end.
-        tc_value = tc_value.replace("K", "")
-        tc_value = float(tc_value)
+        curie_temp = curie_temp.replace("K", "")
 
+        return float(curie_temp)
+
+    def store_2_curie_temp(self):
+        self.curie_temp_tc_mode = self.get_curie_temp(self.filepath_tc_mode)
+        self.curie_temp_j_mode = self.get_curie_temp(self.filepath_j_mode)
+        # Calculate errors between the above 2 values.
+        # The value of the tc_mode is used as a basis.
+        self.absolute_error = self.curie_temp_j_mode - self.curie_temp_tc_mode
+        
+
+    # fuction for test
     def print(self):
-        print(self.filepath_tc_mode)
-        print(self.filepath_j_mode)
+        # print(self.filepath_tc_mode)
+        # print(self.filepath_j_mode)
+        print(self.curie_temp_tc_mode)
+        print(self.curie_temp_j_mode)
 
 
 if __name__ == '__main__':
     filename = "output.dat"
+    # filename = "error_test.txt"
     dirname_tc = "/tc"
     dirname_j = "/j"
     test_instance = TcFromTcandJmode(filename, dirname_tc, dirname_j)
+    # test_instance.print()
+    # test_instance.get_curie_temp(test_instance.filepath_j_mode)
+    test_instance.store_2_curie_temp()
     test_instance.print()
-    test_instance.get_tc(test_instance.filepath_j_mode)
