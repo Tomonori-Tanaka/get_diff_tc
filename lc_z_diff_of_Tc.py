eec4b1ae8c7a2045c56d599cd1cb2df5c163667e
tomorin <tanaka.t.bj@gmail.com>
2021-11-03 17:03:22 +0900

    Unnecessary arguments are removed.

--- lc_z_diff_of_Tc.py
+++ lc_z_diff_of_Tc.py
@@ -9,25 +9,6 @@ import get_diff_tc
 parser = argparse.ArgumentParser(description='Check the difference between Tc in tc and j mode.'
                                              'Directory tree is assumed as:\n'
                                              'lattice_const/atomic_number/(scf_dir)/tc or j')
-
-phelp = 'start lattice constant'
-parser.add_argument('lattice_cconst_start', type=float, help=phelp)
-
-phelp = 'end lattice constant '
-parser.add_argument('lattice_const_end', type=float, help=phelp)
-
-phelp = 'division number (number of the directories) of lattice constant'
-parser.add_argument('division_num_lattice_const', type=int, help=phelp)
-
-phelp = 'start atomic number'
-parser.add_argument('atomic_num_start', type=float, help=phelp)
-
-phelp = 'end atomic number'
-parser.add_argument('atomic_num_end', type=float, help=phelp)
-
-phelp = 'division number (number of the directories) of atomic number'
-parser.add_argument('division_num_atomic_num', type=int, help=phelp)
-
 phelp = 'output file name'
 parser.add_argument('output_file', help=phelp)
 
@@ -35,7 +16,6 @@ phelp = 'error criterion. default is 0.05.'
 parser.add_argument('-ec', '--error_criterion', type=float, default=0.05, help=phelp)
 
 args = parser.parse_args()
-print(args)
 
 # get the path of all tc/ directory
 dir_list = glob.glob('**/tc/', recursive=True)
