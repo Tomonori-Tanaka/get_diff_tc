54d0a31ed5f8487b1211de9437ea252e59eab3e6
tomorin <tanaka.t.bj@gmail.com>
2021-11-03 16:57:24 +0900

    lc_z_diff_of_Tc.py is completed.

--- lc_z_diff_of_Tc.py
+++ lc_z_diff_of_Tc.py
@@ -1,4 +1,10 @@
+"""
+WARNING: This script needs python3.9.
+"""
 import argparse
+import glob
+import os
+import get_diff_tc
 
 parser = argparse.ArgumentParser(description='Check the difference between Tc in tc and j mode.'
                                              'Directory tree is assumed as:\n'
@@ -25,4 +31,34 @@ parser.add_argument('division_num_atomic_num', type=int, help=phelp)
 phelp = 'output file name'
 parser.add_argument('output_file', help=phelp)
 
+phelp = 'error criterion. default is 0.05.'
+parser.add_argument('-ec', '--error_criterion', type=float, default=0.05, help=phelp)
+
 args = parser.parse_args()
+print(args)
+
+# get the path of all tc/ directory
+dir_list = glob.glob('**/tc/', recursive=True)
+
+# remove "tc/" part in the directory names
+try:
+    dir_list.remove('tc/')
+except ValueError:
+    pass
+for i, dir_list_element in enumerate(dir_list):
+    dir_list[i] = dir_list_element.removesuffix('tc/')
+
+# check Tc in each directories
+dir_name_tc = "/tc"
+dir_name_j = "/j"
+root_dir_name = os.getcwd()
+for dir_path in dir_list:
+    os.chdir(dir_path)
+    tc_check_instance = get_diff_tc.TcFromTcandJmode(args.output_file, dir_name_tc, dir_name_j)
+    tc_check_instance.store_2_curie_temp()
+    if abs(tc_check_instance.relative_error) > args.error_criterion:
+        print('Caution! Tc in the below directory has over 5% error.')
+        print(dir_path)
+    os.chdir(root_dir_name)
+
+# print(dir_list)
